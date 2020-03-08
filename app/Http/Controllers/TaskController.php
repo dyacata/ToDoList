747411ce5834d9edcfcc718be36a7b1523a5e422
Djougounah Arnaud <djougounarnaud@gmail.com>
2020-03-08 13:08:32 +0100

    write CRUD

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(){
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title'=>'bail|required|unique:tasks|max:150',
+            'description'=>'required',
+        ]);
+
+        $task = new Task();
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->done = false;
+
+        $task->save;
+
+    }
+
+    public function update(){
+
+    }
+
+    public function delete($id){
+        $task = Task::findOrFail($id);
+        $task->delete();
+    }
+
+    public function show($id){
+        $task = Task::findOrFail($id);
+
+        return response()->json($task);
+    }
 }
